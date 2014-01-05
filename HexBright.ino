17b6cd5b1bb8a6440da87a13a99abbeb0a5f9694
Bernard <github@technologyannex.com>
2014-01-04 18:49:40 -0600

    Final

---
+++ HexBright.ino
@@ -0,0 +1,224 @@
+/* 
+
+  Modified factory firmware for HexBright FLEX 
+  Bernard’s code v1.0 - 4 Jan 2014
+  Original code v2.4  Dec 6, 2012
+  
+  I changed the blinking mode to change the light to high
+  I also changed the time you hold the button down from 500 to 200
+
+  I also added the feature to turn off the light quickly
+  Instead of stepping through low-med-high
+  If light is on, detect button pressed over 200, power off
+  
+*/
+
+#include <math.h>
+#include <Wire.h>
+
+// Settings
+#define OVERTEMP                340
+// Pin assignments
+#define DPIN_RLED_SW            2
+#define DPIN_GLED               5
+#define DPIN_PWR                8
+#define DPIN_DRV_MODE           9
+#define DPIN_DRV_EN             10
+#define APIN_TEMP               0
+#define APIN_CHARGE             3
+// Modes
+#define MODE_OFF                0
+#define MODE_LOW                1
+#define MODE_MED                2
+#define MODE_HIGH               3
+#define MODE_HIGH_ON           4
+#define MODE_HIGH_ON_PREVIEW   5
+
+// State
+byte mode = 0;
+unsigned long btnTime = 0;
+boolean btnDown = false;
+
+
+void setup()
+{
+  // We just powered on!  That means either we got plugged 
+  // into USB, or the user is pressing the power button.
+  pinMode(DPIN_PWR,      INPUT);
+  digitalWrite(DPIN_PWR, LOW);
+
+  // Initialize GPIO
+  pinMode(DPIN_RLED_SW,  INPUT);
+  pinMode(DPIN_GLED,     OUTPUT);
+  pinMode(DPIN_DRV_MODE, OUTPUT);
+  pinMode(DPIN_DRV_EN,   OUTPUT);
+  digitalWrite(DPIN_DRV_MODE, LOW);
+  digitalWrite(DPIN_DRV_EN,   LOW);
+  
+  // Initialize serial busses
+  Serial.begin(9600);
+  Wire.begin();
+  
+  btnTime = millis();
+  btnDown = digitalRead(DPIN_RLED_SW);
+  mode = MODE_OFF;
+
+  Serial.println("Powered on!");
+}
+
+void loop()
+{
+  static unsigned long lastTempTime;
+  unsigned long time = millis();
+  
+  // Check the state of the charge controller
+  int chargeState = analogRead(APIN_CHARGE);
+  if (chargeState < 128)  // Low - charging
+  {
+    digitalWrite(DPIN_GLED, (time&0x0100)?LOW:HIGH);
+  }
+  else if (chargeState > 768) // High - charged
+  {
+    digitalWrite(DPIN_GLED, HIGH);
+  }
+  else // Hi-Z - shutdown
+  {
+    digitalWrite(DPIN_GLED, LOW);    
+  }
+  
+  // Check the temperature sensor
+  if (time-lastTempTime > 1000)
+  {
+    lastTempTime = time;
+    int temperature = analogRead(APIN_TEMP);
+//    Serial.print("Temp: ");
+//    Serial.println(temperature);
+//    SENSORS   
+//    Serial.print("Charge: "); 
+//    Serial.println(chargeState);
+    if (temperature > OVERTEMP && mode != MODE_OFF)
+    {
+      Serial.println("Overheating!");
+
+      for (int i = 0; i < 6; i++)
+      {
+        digitalWrite(DPIN_DRV_MODE, LOW);
+        delay(100);
+        digitalWrite(DPIN_DRV_MODE, HIGH);
+        delay(100);
+      }
+      digitalWrite(DPIN_DRV_MODE, LOW);
+
+      mode = MODE_LOW;
+    }
+  }
+
+  // Do whatever this mode does
+  switch (mode)
+  {
+  case MODE_HIGH_ON:
+  case MODE_HIGH_ON_PREVIEW:
+//    digitalWrite(DPIN_DRV_EN, (time%300)<75);
+    digitalWrite(DPIN_DRV_EN, HIGH);
+    break;
+  }
+  
+  // Periodically pull down the button's pin, since
+  // in certain hardware revisions it can float.
+  pinMode(DPIN_RLED_SW, OUTPUT);
+  pinMode(DPIN_RLED_SW, INPUT);
+  
+  // Check for mode changes
+  byte newMode = mode;
+  byte newBtnDown = digitalRead(DPIN_RLED_SW);
+  switch (mode)
+  {
+  case MODE_OFF:
+    if (btnDown && !newBtnDown && (time-btnTime)>20)
+      newMode = MODE_LOW;
+//    if (btnDown && newBtnDown && (time-btnTime)>500)
+    if (btnDown && newBtnDown && (time-btnTime)>200)
+      newMode = MODE_HIGH_ON_PREVIEW;
+    break;
+  case MODE_LOW:
+    if (btnDown && !newBtnDown && (time-btnTime)>50)
+      newMode = MODE_MED;
+    if (btnDown && !newBtnDown && (time-btnTime)>200)
+      newMode = MODE_OFF;
+    break;
+  case MODE_MED:
+    if (btnDown && !newBtnDown && (time-btnTime)>50)
+      newMode = MODE_HIGH;
+    if (btnDown && !newBtnDown && (time-btnTime)>200)
+      newMode = MODE_OFF;
+    break;
+  case MODE_HIGH:
+    if (btnDown && !newBtnDown && (time-btnTime)>50)
+      newMode = MODE_OFF;
+    break;
+  case MODE_HIGH_ON_PREVIEW:
+    // This mode exists just to ignore this button release.
+    if (btnDown && !newBtnDown)
+      newMode = MODE_HIGH_ON;
+    break;
+  case MODE_HIGH_ON:
+    if (btnDown && !newBtnDown && (time-btnTime)>50)
+      newMode = MODE_OFF;
+    break;
+  }
+
+  // Do the mode transitions
+  if (newMode != mode)
+  {
+    switch (newMode)
+    {
+    case MODE_OFF:
+      Serial.println("Mode = off");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, LOW);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      digitalWrite(DPIN_DRV_EN, LOW);
+      break;
+    case MODE_LOW:
+      Serial.println("Mode = low");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      analogWrite(DPIN_DRV_EN, 64);
+      break;
+    case MODE_MED:
+      Serial.println("Mode = medium");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, LOW);
+      analogWrite(DPIN_DRV_EN, 255);
+      break;
+    case MODE_HIGH:
+      Serial.println("Mode = high");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, HIGH);
+      analogWrite(DPIN_DRV_EN, 255);
+      break;
+    case MODE_HIGH_ON:
+    case MODE_HIGH_ON_PREVIEW:
+      Serial.println("Mode = HIGH_ON");
+      pinMode(DPIN_PWR, OUTPUT);
+      digitalWrite(DPIN_PWR, HIGH);
+      digitalWrite(DPIN_DRV_MODE, HIGH);
+      break;
+    }
+
+    mode = newMode;
+  }
+
+  // Remember button state so we can detect transitions
+  if (newBtnDown != btnDown)
+  {
+    btnTime = time;
+    btnDown = newBtnDown;
+    delay(50);
+  }
+}
+
+
